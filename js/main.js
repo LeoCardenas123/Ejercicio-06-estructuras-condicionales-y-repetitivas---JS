9a2f5a18f7391d867b870331bd0482dcb808ce8a
leonel <leocardenas2412@gmail.com>
2025-06-12 17:21:51 -0300

    a la noche sigo porque tengo que ir a trabajar

--- js/main.js
+++ js/main.js
@@ -2,27 +2,32 @@ console.error('EJERCICIO A')
 
 var cantidadDeGatos 
 
-var cantidadIngresada = parseFloat(prompt('Cuantos gatos quieres?')) 
+var cantidadIngresada = 10 /* parseFloat(prompt('Cuantos gatos quieres?'))  */
+
+var emojiDeGato
+
+var textoSalida = 'Gato #' + cantidadDeGatos + ' ' + emojiDeGato
 
 /* Declaro las variables cantidadDeGatos, y cantidadIngresada , la cual tiene un prompt para que sea pueda ser elegido por el usuario la cantidad de lineas que desea imprimir en la consola*/
 
 
 /* asigno el valor 1 a cantidadDeGatos, y le digo al FOR, que cada vez que CantidadDeGatos sea menor o igual a cantidadIngresada añada una linea mas */
-for(cantidadDeGatos = 1; cantidadDeGatos <=  cantidadIngresada; cantidadDeGatos++){
+
+
+/* for(cantidadDeGatos = 1; cantidadDeGatos <=  cantidadIngresada; cantidadDeGatos++){
     
-    var emojiDeGato 
-    if(cantidadDeGatos % 2 == 0){
-        emojiDeGato = '3'      
+    if((cantidadDeGatos % 3 = 0) ){
+        emojiDeGato = 23
     }
-    else if(cantidadDeGatos % 3 == 0){
-        emojiDeGato = '2'      
-    }
-    else{
-        emojiDeGato = '1'      
-    }
-    
-    var textoSalida = 'Gato #' + cantidadDeGatos + ' ' + emojiDeGato
+
+    textoSalida = 'Gato #' + cantidadDeGatos + ' ' + emojiDeGato
+
     console.log(textoSalida)
     
-    /* tengo que modificar algunos detalles pero esta seria la base que usaria para elegir cual es el emojin que se va a imprimir en la consola */
+} */
+
+do{
+    console.log(textoSalida)
+    cantidadDeGatos ++
 }
+while(cantidadDeGatos <= cantidadIngresada)
