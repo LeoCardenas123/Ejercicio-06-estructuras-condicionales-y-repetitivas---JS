39c47f68a944b54e8b2ef33e5dc13758c0c7565c
leonel <leocardenas2412@gmail.com>
2025-06-13 13:33:26 -0300

    ya terminé la estructura para imprimir las lineas en consola dependiendo de cuantas quieras, ahora voy a crear el algoritmo para acomodar los emojis

---
+++ js/ejercicioB.js
@@ -0,0 +1,15 @@
+console.error('EJERCICIO B')
+var numeroDeGatos 
+var emojiGato = ' 🐈'
+var cantidadDePasos
+var resultado
+
+var cantidadDeFilas = 5
+numeroDeGatos = cantidadDeFilas
+
+
+for(numeroDeGatos = 1; numeroDeGatos <= cantidadDeFilas; numeroDeGatos++){
+    resultado = 'Gato #' + numeroDeGatos +':'+ emojiGato + ' emoji' 
+    console.log(resultado)
+}
+
