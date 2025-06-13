dc2adeeb8c0c0d63a195c8d6466be6dfe8435673
leonel <leocardenas2412@gmail.com>
2025-06-13 13:40:23 -0300

    trabajo en el algoritmo para determinar la cantidad de pasos en cada linea

--- js/ejercicioB.js
+++ js/ejercicioB.js
@@ -1,15 +1,16 @@
 console.error('EJERCICIO B')
 var numeroDeGatos 
 var emojiGato = ' 🐈'
-var cantidadDePasos
+var cantidadDePasos = 5
+var emojiPasos = ' 🐾'
 var resultado
-
 var cantidadDeFilas = 5
+
 numeroDeGatos = cantidadDeFilas
 
-
 for(numeroDeGatos = 1; numeroDeGatos <= cantidadDeFilas; numeroDeGatos++){
-    resultado = 'Gato #' + numeroDeGatos +':'+ emojiGato + ' emoji' 
+
+    resultado = 'Gato #' + numeroDeGatos +':' + emojiGato + emojiPasos 
     console.log(resultado)
 }
 
