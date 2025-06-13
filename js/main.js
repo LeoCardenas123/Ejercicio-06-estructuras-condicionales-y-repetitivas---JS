2dfc87e035d70cd493f533928bb136db00a50b47
leonel <leocardenas2412@gmail.com>
2025-06-13 02:28:31 -0300

    estoy haciendo el algoritmo para determinar la posicion de cada emoji

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var cantidadIngresada = 10 /* parseFloat(prompt('Cuantos gatos quieres?'))  */
 
 var emojiDeGato
 
-var textoSalida = 'Gato #' + cantidadDeGatos + ' ' + emojiDeGato
+var textoSalida 
 
 /* Declaro las variables cantidadDeGatos, y cantidadIngresada , la cual tiene un prompt para que sea pueda ser elegido por el usuario la cantidad de lineas que desea imprimir en la consola*/
 
@@ -14,20 +14,22 @@ var textoSalida = 'Gato #' + cantidadDeGatos + ' ' + emojiDeGato
 /* asigno el valor 1 a cantidadDeGatos, y le digo al FOR, que cada vez que CantidadDeGatos sea menor o igual a cantidadIngresada añada una linea mas */
 
 
-/* for(cantidadDeGatos = 1; cantidadDeGatos <=  cantidadIngresada; cantidadDeGatos++){
-    
-    if((cantidadDeGatos % 3 = 0) ){
-        emojiDeGato = 23
+for(cantidadDeGatos = 1; cantidadDeGatos <= cantidadIngresada; cantidadDeGatos++){
+
+    if(cantidadDeGatos % 1.5 == 1){
+        var emojiDeGato = 1111
+    }
+    else if(cantidadDeGatos % 3 == 0){
+        emojiDeGato = 00 
+    }
+    else{
+        emojiDeGato = 55
     }
 
     textoSalida = 'Gato #' + cantidadDeGatos + ' ' + emojiDeGato
 
     console.log(textoSalida)
     
-} */
+} 
 
-do{
-    console.log(textoSalida)
-    cantidadDeGatos ++
-}
-while(cantidadDeGatos <= cantidadIngresada)
+
